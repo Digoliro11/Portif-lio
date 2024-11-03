dd9ce73caaf904c80b8ac4aa3845e6ce9f007047
Rodrigo Gerçóssimo Mattos <133182774+Digoliro11@users.noreply.github.com>
2024-11-03 00:19:20 +0000

    botão de voltar ao topo e apresentação

--- java.js
+++ java.js
@@ -3,3 +3,19 @@ let checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('click', () => {
     document.body.classList.toggle('claro');
 });
+
+window.onscroll = function() {
+    const topoBtn = document.getElementById("topoBtn");
+    if (window.scrollY > 300) { 
+        topoBtn.style.display = "block";
+    } else {
+        topoBtn.style.display = "none";
+    }
+};
+
+document.getElementById("topoBtn").addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
